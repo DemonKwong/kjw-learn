b1ed39d98f29b6e1bc57aaddd8caea9e01c5e7e9
kuangjiewen <kuangjiewen@grgbanking.com>
2019-02-01 10:43:47 +0800

    快速排序

--- src/main/java/algorithm/KuaiSuSort.java
+++ src/main/java/algorithm/KuaiSuSort.java
@@ -2,10 +2,9 @@ package algorithm;
 
 public class KuaiSuSort {
 	public static void main(String[] args) {
-		int[] a ={1,2,3,4,5,6,7};
-		int i=5;
-		System.out.println(a[i--]);
-		System.out.println(i);
+		int []arr = {8,6,4,7,3,5};
+		sort(arr);
+		System.out.println(arr);
 
 	}
 
@@ -14,30 +13,43 @@ public class KuaiSuSort {
 		if(arr == null || arr.length == 0){
 			return;
 		}
+		quickSort(arr,0,arr.length-1);
 	}
 
+	/**
+	 * 核心思想：随机选个数作为比较数，此处选第一个，将范围内的比 比较数小的数字放在左边，大的放在右边。
+	 *             排序完成后用递归对左右两边的继续运用此算法
+	 * */
 	private static void quickSort(int[] arr,int left,int right){
-		if(left == right){
+		int l = left;
+		int r = right;
+		if(left >= right){
 			return;
 		}
 		int temp = arr[left];
 		int emptyIndex = left;
-		while (left != right) {
+		while (left < right) {
 			if(emptyIndex == left){
 				if(arr[right]<temp){
 					arr[left] = arr[right];
 					emptyIndex = right;
+					left++;
+				}else{
+					right--;
 				}
-				left++;
 			}else if(emptyIndex == right){
 				if(arr[left]>temp){
 					arr[right] = arr[left];
 					emptyIndex = left;
+					right--;
+				}else{
+					left++;
 				}
-				right--;
 			}
 		}
 		arr[left] = temp;
+		quickSort(arr,l,left-1);
+		quickSort(arr,left+1,r);
 	}
 
 }
